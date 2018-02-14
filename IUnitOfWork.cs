adc9c242c98690d76580d81bf1affee8caabe402
sunnypre <35748775+sunnypre@users.noreply.github.com>
2018-02-14 15:47:17 +0100

    Add files via upload

---
+++ IUnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramConstruction
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IInstitutionRepository Institutions { get; }
+        RoomRepository Rooms { get; }
+        Repository<Person> People { get; }
+        Repository<PersonsInInstView> PeopleInInst {get;}
+        
+
+       int Complete();
+
+    }
+}
---
+++ UnitOfWork.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramConstruction
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly EhlContactsEntities _context;
+
+        public IInstitutionRepository Institutions { get; private set; }
+
+        public RoomRepository Rooms { get; private set; }
+        public Repository<Person> People { get; private set; }
+        public Repository<PersonsInInstView> PeopleInInst { get; private set; }
+        
+
+        public UnitOfWork(EhlContactsEntities context)
+        {
+            _context = context;
+            Rooms = new RoomRepository(_context);
+            Institutions = new InstitutionRepository(_context);
+            People = new Repository<Person>(_context);
+            PeopleInInst = new Repository<PersonsInInstView>(_context);
+            
+           
+        }
+        public int Complete()
+        {
+            return _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+        
+    }
+}
